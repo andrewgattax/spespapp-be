730da6a44cf4dbeafd5c3a5b87d70bb49535bf58
Andrea Gatta <x@ndrw.cat>
2026-04-23 18:09:00 +0200

    first one

---
+++ src/main/java/it/trinex/spespapp/SpespappApplication.java
@@ -0,0 +1,13 @@
+package it.trinex.spespapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpespappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpespappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/trinex/spespapp/SpespappApplicationTests.java
@@ -0,0 +1,13 @@
+package it.trinex.spespapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpespappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
