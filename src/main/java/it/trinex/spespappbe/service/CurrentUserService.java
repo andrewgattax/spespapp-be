e4bf647e8cf68d4d46375910cbf45dda2db7734a
Andrea Gatta <x@ndrw.cat>
2026-04-24 23:54:12 +0200

    current user service

---
+++ src/main/java/it/trinex/spespappbe/service/CurrentUserService.java
@@ -0,0 +1,34 @@
+package it.trinex.spespappbe.service;
+
+import it.trinex.spespappbe.model.SpespappUser;
+import it.trinex.spespappbe.repo.UserRepo;
+import it.trinex.spespappbe.security.JwtUserPrincipal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CurrentUserService {
+
+    private final UserRepo userRepo;
+
+    public Optional<JwtUserPrincipal> getCurrentPrincipal() {
+        Optional<JwtUserPrincipal> userPrincipal = Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+                .filter(auth -> auth.getPrincipal() instanceof JwtUserPrincipal)
+                .map(auth -> (JwtUserPrincipal) auth.getPrincipal());
+        return userPrincipal;
+    }
+
+    public Optional<SpespappUser> getCurrentUser() {
+        Optional<JwtUserPrincipal>  userPrincipal = getCurrentPrincipal();
+        if(userPrincipal.isPresent()) {
+            return userRepo.findByUsername(userPrincipal.get().getUsername());
+        } else {
+            return Optional.empty();
+        }
+    }
+
+}
