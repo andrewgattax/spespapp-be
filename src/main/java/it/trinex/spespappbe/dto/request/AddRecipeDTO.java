f112e382670fe87bad45432388eb44acb39439ed
Manuel-Montaruli <manuelmontaruli@gmail.com>
2026-04-25 02:46:56 +0200

    Added Recipe DTO

---
+++ src/main/java/it/trinex/spespappbe/dto/request/AddRecipeDTO.java
@@ -0,0 +1,22 @@
+package it.trinex.spespappbe.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddRecipeDTO {
+    @NotBlank
+    private String name;
+
+    @NotEmpty
+    private List<String> ingredientNames;
+}
---
+++ src/main/java/it/trinex/spespappbe/dto/request/UpdateRecipeDTO.java
@@ -0,0 +1,18 @@
+package it.trinex.spespappbe.dto.request;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateRecipeDTO {
+    @NotEmpty
+    private List<String> ingredientNames;
+}
