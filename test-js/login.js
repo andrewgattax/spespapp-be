a293e9a834c10f196ebd139837638ae63a9be6f6
Andrea Gatta <x@ndrw.cat>
2026-04-25 02:39:28 +0200

    edited js

--- test-js/login.js
+++ test-js/login.js
@@ -5,7 +5,7 @@ import base64url from 'base64url';
 
 // Configuration
 const API_BASE_URL = 'http://localhost:8080/api/auth';
-const USERNAME = 'manu';
+const USERNAME = process.argv[2] || 'manu';
 
 async function testAuthentication() {
   try {
